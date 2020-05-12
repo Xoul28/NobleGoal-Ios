31f6ae43378964d24de85e1188bb4a74ba23aa1a
Mike Chernikov <Exoberfox@gmail.com>
2020-05-12 22:35:29 +0300

    Removed all swiftUI related classes

--- NobleGoal/NobleGoal/ViewController.swift
+++ NobleGoal/NobleGoal/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  NobleGoal
-//
-//  Created by Mike Chernikov on 12.05.2020.
-//  Copyright © 2020 Mike Chernikov. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,6 +5,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
